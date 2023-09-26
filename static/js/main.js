89a35c25ade3e85567897af3dc0caf46c27b6521
Heidy Cardenas <140125367+HeidyCardenas1903@users.noreply.github.com>
2023-09-26 16:48:04 -0500

    Login estilos

--- static/js/main.js
+++ static/js/main.js
@@ -1,4 +1,18 @@
+// Functions
 function panelLateral(){
     const panel = document.querySelector(".panel-lateral");
     panel.classList.toggle('active');
 }
+
+const password = document.querySelector("#contraseña");
+const eye = document.querySelector(".ojo");
+
+eye.addEventListener("click", function() {
+  if (password.type === "password") {
+    password.type = "text";
+    eye.classList.add("active");
+  } else {
+    password.type = "password";
+    eye.classList.remove("active");
+  }
+});
