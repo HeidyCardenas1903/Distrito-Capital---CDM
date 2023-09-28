a707049e493cb0487bc3ab571fb4b43dff4146c5
vivian <carohinca1997@gmail.com>
2023-09-28 10:23:13 -0500

    " "

--- Herramientas/manzanascuidado.sql
+++ Herramientas/manzanascuidado.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 28-09-2023 a las 17:05:33
+-- Tiempo de generación: 28-09-2023 a las 17:21:56
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
 
